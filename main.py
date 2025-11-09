3b0d160bed35af4dbee79e4c1971dc66bee8cc4e
Danteskywo <skywo@mail.ru>
2025-11-09 11:50:38 +0300

    first commit

--- main.py
+++ main.py
@@ -3,70 +3,74 @@ from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import uuid
-
+from fastapi import FastAPI, Body, status
+from fastapi.responses import JSONResponse, FileResponse
+ 
 class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
         self.id = str(uuid.uuid4())
-people = [Person("Ilya", 33), Person("Robert", 43), Person("Marta", 53)]
+ 
 
+people = [Person("Ilya", 33), Person("Robert", 17), Person("Marta", 28)]
+ 
 
 def find_person(id):
-    for person in people:
+   for person in people: 
         if person.id == id:
-            return person
-    return None
-
-
+           return person
+   return None
+ 
 app = FastAPI()
-
+ 
 @app.get("/")
 async def main():
     return FileResponse("public/index.html")
-
+ 
 @app.get("/api/users")
 def get_people():
     return people
-
+ 
 @app.get("/api/users/{id}")
 def get_person(id):
     person = find_person(id)
-    print (person)
-
-    if person == None:
+    print(person)
+    if person==None:  
         return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            content={"message":"Пользователь не найден!"}
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Пользователь не найден" }
         )
     return person
-
+ 
+ 
 @app.post("/api/users")
-def create_person(data = Body()):
+def create_person(data  = Body()):
     person = Person(data["name"], data["age"])
     people.append(person)
     return person
-
+ 
 @app.put("/api/users")
-def edit_person(data = Body()):
+def edit_person(data  = Body()):
     person = find_person(data["id"])
-    if person == None:
+    if person == None: 
         return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND,
-            content={"message": "Пользователь не найден!"}
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Пользователь не найден" }
         )
     person.age = data["age"]
     person.name = data["name"]
     return person
-
+ 
+ 
 @app.delete("/api/users/{id}")
 def delete_person(id):
     person = find_person(id)
 
     if person == None:
         return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND,
-            content={"message": "Пользователь не найден!"}
+                status_code=status.HTTP_404_NOT_FOUND, 
+                content={ "message": "Пользователь не найден" }
         )
     people.remove(person)
     return person
